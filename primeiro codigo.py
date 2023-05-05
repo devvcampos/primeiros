d4473990ea132be6917031dc140f558e2120a128
Pedro Campos <90489838+DeoPr@users.noreply.github.com>
2023-05-04 22:17:09 -0300

    Add files via upload

---
+++ primeiro codigo.py
@@ -0,0 +1,9 @@
+pergunta = input("Qual é o melhor filme do Shrek? ")
+
+if pergunta == "shrek 1" or pergunta == "shrek 2":
+    print("Resposta certa!")
+elif pergunta == "shrek 3"  or pergunta == "shrek 4":  
+    print("Resposta errada :/")
+else:
+    print("Resposta invalida")           
+           
